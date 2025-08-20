003d570b57f41238f627b6a184d632a621792f1b
junior_Miguel <juniorcastilloy@gmail.com>
2025-08-19 23:32:48 -0500

    initial:contacts-firebase-angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'contacts-angular-17dcd',
+        appId: '1:815795356010:web:6693be24cc28ba59996513',
+        storageBucket: 'contacts-angular-17dcd.firebasestorage.app',
+        apiKey: 'AIzaSyDsBHGDS2CBdiQY3CyfDcAuf0SKm4HTwo8',
+        authDomain: 'contacts-angular-17dcd.firebaseapp.com',
+        messagingSenderId: '815795356010',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
