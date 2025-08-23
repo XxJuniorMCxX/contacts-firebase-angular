d9fd21814de5a2039b6d91d439a8dd9d762a768d
junior_Miguel <juniorcastilloy@gmail.com>
2025-08-22 23:22:30 -0500

    initial:creating-components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,20 +6,17 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from '@env/environment.development';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
     provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'contacts-angular-17dcd',
-        appId: '1:815795356010:web:6693be24cc28ba59996513',
-        storageBucket: 'contacts-angular-17dcd.firebasestorage.app',
-        apiKey: 'AIzaSyDsBHGDS2CBdiQY3CyfDcAuf0SKm4HTwo8',
-        authDomain: 'contacts-angular-17dcd.firebaseapp.com',
-        messagingSenderId: '815795356010',
-      })
+      initializeApp(
+        environment.firebase
+      )
     ),
     provideFirestore(() => getFirestore()),
   ],
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+    firebase:{ //esto contiene la configuración de Firebase
+        projectId: 'contacts-angular-17dcd',
+        appId: '1:815795356010:web:6693be24cc28ba59996513',
+        storageBucket: 'contacts-angular-17dcd.firebasestorage.app',
+        apiKey: 'AIzaSyDsBHGDS2CBdiQY3CyfDcAuf0SKm4HTwo8',
+        authDomain: 'contacts-angular-17dcd.firebaseapp.com',
+        messagingSenderId: '815795356010'
+    }
+};
+
