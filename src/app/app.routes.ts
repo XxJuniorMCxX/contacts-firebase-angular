5f947683322a3d9c3e0114deae8e87d7c1b28867
junior_Miguel <juniorcastilloy@gmail.com>
2025-08-26 23:46:40 -0500

    feat:angular-toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+    path: '',//este es el path de la aplicacion
+    redirectTo: '/contacts',//Redirect a la ruta de contactos
+    pathMatch: 'full',//redirige a la ruta/contacts cuando la ruta es vacia
+  },
+
+  {
+    path:'contacts',//esta es la ruta /contacts
+    loadChildren: () => import('./features/contacts/contacts.routes')
+    },
+
+  {
+    path: '**',//cualquier otra ruta no definida 
+    redirectTo: '/contacts',//redirige a la ruta / contacts
+    
+  },
+
+];
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,31 +1,36 @@
 import { Component } from '@angular/core';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const MaterialModules = [
-  MatIconModule,
-  MatButtonModule,
-  MatToolbarModule
-];
-
+const MaterialModules = [MatIconModule, MatButtonModule, MatToolbarModule];
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
   imports: [MaterialModules],
   template: `
-    <mat-toolbar>
-      <a>
+    <mat-toolbar class="primary-color">
+      <a mat-button>
         <mat-icon>home</mat-icon>
         <span>Home</span>
       </a>
-    
+      <a mat-button>
+        <mat-icon>format_list_bulleted</mat-icon>
+        <span>Contactos</span>
+      </a>
+      <span class="spacer"></span>
+      <a mat-button (click)="emitClick()">
+        <mat-icon color="primary">add_box</mat-icon>
+        <span>Nuevo</span>
+      </a>
     </mat-toolbar>
   `,
-  styles: ``
+  styles: ``,
 })
 export class ToolbarComponent {
-
+  emitClick() {
+    console.log('click');
+  }
 }
