51f0208bf3fbe38f444934bd7e358186cab93c0d
junior_Miguel <juniorcastilloy@gmail.com>
2025-09-02 18:12:17 -0500

    feat:angular-grid-list

---
+++ src/app/features/contacts/contac.interface.ts
@@ -0,0 +1,16 @@
+import { Timestamp } from "@angular/fire/firestore"
+
+export type ColumnKeys<T> = Array<keyof T>;
+//ColumnKeys<T> es un tipo generico que representa las claves de un objeto como un array
+//Keyof<T> es un tipo que representa las claves de un objeto y devuelve un objeto tambien
+export interface Contact {
+  id: number,
+  name: string,
+  email: string,
+  phone: string,
+  country: string,
+  actions:string,
+  created: Timestamp,
+  updated: Timestamp
+}
+//una interface define la forma de un objeto
--- src/app/features/contacts/contact.service.ts
+++ src/app/features/contacts/contact.service.ts
@@ -1,2 +1,13 @@
 //separar lógica de negocio como crear, editar y eliminar contactos
 import { Inject,Injectable }from "@angular/core";
+import { DocumentData,Firestore,addDoc,collection,deleteDoc,doc,updateDoc,
+    getDoc,DocumentReference,orderBy,query,collectionData
+ } from "@angular/fire/firestore";
+import { Observable } from "rxjs";//son objetos que emiten valores en el tiempo y se utilizan para manejo de eventos asíncronos
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+}
+//nos permite inyectar el servicio en otros componentes de nuestra aplicación
