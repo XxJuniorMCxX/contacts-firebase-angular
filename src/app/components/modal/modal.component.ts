27a28acd434f365b9f7bf8d3e8a6b1741189ac7f
junior_Miguel <juniorcastilloy@gmail.com>
2025-09-04 20:03:46 -0500

    feat:modal-add-update-delete

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
-import { MatFormField,MatLabel } from '@angular/material/form-field';
+import { MatFormFieldModule,MatLabel } from '@angular/material/form-field';
+import { MAT_DIALOG_DATA,MatDialogModule } from '@angular/material/dialog';
 
-const materialModules=[MatInputModule];
+import { FormBuilder,FormGroup,ReactiveFormsModule,Validator } from '@angular/forms';
+
+const materialModules=[ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatDialogModule];
 @Component({
   selector: 'app-modal',
   standalone: true,
@@ -10,6 +13,20 @@ const materialModules=[MatInputModule];
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  
+  contactForm!: FormGroup;//es una variable que va a tener un formulario de contacto
+  private readonly _formBuilder=inject(FormBuilder);
+  private readonly _matDialog=inject(MAT_DIALOG_DATA);
+  private readonly _contactSvc=inject
+  
+  
+  
+  ngOnInit(): void {
+      
+  }
 
+  getTitle():string{
+    return this._matDialog.data?'Update Contact':'New Contact';
+  }
 }
